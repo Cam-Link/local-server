b9a67a263cbcf35a8c853b6b74c6441c3a44000e
unknown <hnfsrj@gmail.com>
2024-03-31 14:55:54 -0900

    Add initial structure of the project

---
+++ camlink/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("start/", views.start),
+    path("link/", views.link),
+    path("stream/", views.stream),
+    path("refresh/", views.refresh),
+    path("play/", views.play),
+]
--- camlink/views.py
+++ camlink/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
+
+
+def start():
+  pass
+
+
+
+
+
+
+
+def link():
+  pass
+
+
+
+
+
+
+
+def stream():
+  pass
+
+
+
+
+
+
+def refresh():
+  pass
+
+
+
+
+
+def play():
+  pass
