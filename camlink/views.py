69c829d8cc5e6b9bd11de1993ee1ab8ed5e8e56b
unknown <hnfsrj@gmail.com>
2024-04-02 12:04:11 -0900

    Add play view

--- camlink/views.py
+++ camlink/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
+import os
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, FileResponse
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
@@ -39,6 +40,33 @@ def refresh():
 
 
 
+@csrf_exempt
+def play(request):
+  try:   
 
-def play():
-  pass
+    if request.method == "GET":
+      try:
+          
+        data = json.loads(request.body)
+
+        uid = data['uid']
+        cid = data['cid']
+
+        #return the requested chunk of video
+        
+        
+        path = os.getcwd() + f"/camlink/videos/{uid}/{cid}.webm"
+
+        chunk = open(path, 'rb')
+
+        return FileResponse(chunk, content_type='video/webm')
+
+      except Exception as e:
+        return JsonResponse({'msg':str(e)})
+
+    else:
+        return JsonResponse({'msg':"method not supported"})
+
+
+  except:
+    return JsonResponse({'msg':"Unexpected error"})
