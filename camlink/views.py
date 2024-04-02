19d437abd521a9c72f44f59d0c8e3413f0a741be
unknown <hnfsrj@gmail.com>
2024-04-02 08:40:58 -0900

    update gitignore

--- camlink/views.py
+++ camlink/views.py
@@ -4,6 +4,8 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from camlink.models import Link
+
 # Create your views here.
 
 
@@ -16,8 +18,37 @@ def start():
 
 
 
-def link():
-  pass
+def link(request):
+  try:   
+
+    if request.method == "POST":
+      try:
+          
+        data = json.loads(request.body)
+
+        code = data['code']
+
+        #return error message if the code doesn't match with the code variable here
+
+        new_link = Link()
+
+        new_link.save()
+
+        request.session['uid'] = new_link.id
+
+        
+
+        return JsonResponse(data)
+
+      except Exception as e:
+          return JsonResponse({'msg':str(e)})
+
+    else:
+        return JsonResponse({'msg':"method not supported"})
+
+
+  except:
+    return JsonResponse({'msg':"Unexpected error"})
 
 
 
