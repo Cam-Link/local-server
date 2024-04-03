a5daedafab4dfb062aa35590946439514544f3ab
unknown <hnfsrj@gmail.com>
2024-04-03 10:05:14 -0900

    Add refresh basics

--- camlink/views.py
+++ camlink/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from camlink.models import Link
 
 import json
 from django.http import JsonResponse
@@ -32,9 +33,38 @@ def stream():
 
 
 
+@csrf_exempt
+def refresh(request):
+  try:   
 
-def refresh():
-  pass
+    if request.method == "GET":
+      try:
+        
+        # peers = request.session.get("peers",0)
+
+        peers=0
+
+        last_id = Link.objects.all().order_by('-id')[0]
+
+        if peers == last_id:
+          return JsonResponse({'msg':"no new link"})
+
+        new_links=[last_id+1]
+
+        while(new_links[-1] <= last_id):
+          new_links.append(new_links[-1] + 1)
+
+        return JsonResponse({'msg':'success','add':new_links})
+
+      except Exception as e:
+        return JsonResponse({'msg':str(e)})
+
+    else:
+        return JsonResponse({'msg':"method not supported"})
+
+
+  except:
+    return JsonResponse({'msg':"Unexpected error"})
 
 
 
