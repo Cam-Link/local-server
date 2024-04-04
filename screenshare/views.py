8631459a4fc32f5e478c86f1be5d72a62be94c08
unknown <hnfsrj@gmail.com>
2024-04-04 04:04:54 -0900

    Complete play view in screenshare

--- screenshare/views.py
+++ screenshare/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
+import os
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, FileResponse
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
@@ -42,7 +43,20 @@ def play(request):
         
         num = request.session.get("num",0)
 
+        #return the requested c
         
+        path = os.getcwd() + f"/screenshare/screen/{num}.webm"
+
+        try:
+
+          chunk = open(path, 'rb')
+        except:
+          return JsonResponse({'msg':"pending"})
+
+        request.session['num'] = int(num) + 1
+
+        return FileResponse(chunk, content_type='video/webm')
+
 
       except Exception as e:
         return JsonResponse({'msg':str(e)})
