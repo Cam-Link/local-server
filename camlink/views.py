ed28433969719b79c947a89860fa0ef5c96803da
unknown <hnfsrj@gmail.com>
2024-04-04 22:37:41 +0300

    Fix play view merge issue

--- camlink/views.py
+++ camlink/views.py
@@ -159,9 +159,36 @@ def refresh(request):
 
 
 
-def play():
-  pass
+@csrf_exempt
+def play(request):
+  try:   
 
+    if request.method == "GET":
+      try:
+          
+        data = json.loads(request.body)
+
+        uid = data['uid']
+        cid = data['cid']
+
+        #return the requested chunk of video
+        
+        
+        path = os.getcwd() + f"/camlink/videos/{uid}/{cid}.webm"
+
+        chunk = open(path, 'rb')
+
+        return FileResponse(chunk, content_type='video/webm')
+
+      except Exception as e:
+        return JsonResponse({'msg':str(e)})
+
+    else:
+        return JsonResponse({'msg':"method not supported"})
+
+
+  except:
+    return JsonResponse({'msg':"Unexpected error"})
 
 
 
