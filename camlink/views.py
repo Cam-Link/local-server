4bda251e1e700e8c61f289c614204c7aa69e7270
SenaMekonin <senamekonin19@gmail.com>
2024-04-05 22:21:30 +0300

    Finish Start

--- camlink/views.py
+++ camlink/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-import uuid
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -9,29 +8,33 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 
 def start(request):
+  try:
     if request.method == 'POST':
       
-      # To Generate unique link code based on Host ID
-        link_code = str(uuid.uuid1())
+        # Generate link code 
         
-      # Set Code variable to the link code
-        code = link_code
+        link_code = "ab12" 
         
-      # The Response data
-        response_data = {
-          'msg': 'success',
-          'code' : link_code
-        }
-      
-      # Cookies with 'Peers : 0'
-      
-        response = JsonResponse(response_data)
-        response.set_cookie('Peers', '0')
-      
-    
+        if link_code:   
+          
+          response_data = {
+                        'msg': 'success',
+                        'code' : link_code
+                    }
+                   
+          response = JsonResponse(response_data)
+          response.set_cookie('Peers', '0')
+          return response
+        
+        else:
+              return JsonResponse({'error': 'Link code is missing.'}, status=400)
+              
     else:
         # Return an error if the request method is not POST
-        return JsonResponse({'error': 'Method is not allowed.'}, status = 405)
+        return JsonResponse({'error': 'Method is not allowed.'}, status=405)
+      
+  except Exception as e:
+    return JsonResponse({'error': f'Unexpected error: {str(e)}'})
 
 
 
